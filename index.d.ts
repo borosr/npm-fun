d2b8fe4d335c987925e64a6f2b121c8117d1e83a
Robert Boros <robbie13.boros@gmail.com>
2022-04-23 09:40:28 +0200

    update package

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+import HelloWorld from "./src/main";
+
+declare module "borosr_npm_test" {
+    export default HelloWorld
+}
---
+++ index.js
@@ -0,0 +1,3 @@
+import HelloWorld from "./src/main";
+
+exports.HelloWorld = HelloWorld;
